c6540abe3033cc22674d70788074ac8686d64293
nandagurram16 <nandagurram168@gmail.com>
2024-12-23 13:04:52 +0530

    'one'

---
+++ one.js
@@ -0,0 +1,168 @@
+// var n=2;
+// created function and allocate name to function as sqaure
+// function square(num){
+//     created variable in side the function as ans=num*num 
+//     var ans=num*num
+//     return the ans
+//     return ans
+// }
+
+// square2 is a variable inside the sq2 we r allocated inside the num parameter as 2
+// so squarea(2) 
+// so num=2,var ans=num*num
+// so ans=4
+// so square2=4
+// var square2=square(n)
+// square2 is a variable inside the sq4 we r allocated inside the num parameter as 2
+// so squarea(4) 
+// so num=2,var ans=num*num
+// so ans=16
+// so square4=16
+// var square4=square(4)
+
+// console.log(square2)
+// console.log(square4)
+// =============================================
+
+// var x=7
+
+// var getname =()=>{
+//     console.log('hello js')
+// }
+// console.log(x)
+// console.log(getname)
+// getname()
+// =========================================
+// function a(){
+//     var x=10;
+//     console.log(x)
+// }
+
+// function b(){
+//     var x=100;
+//     console.log(x)
+// }
+// var x=1;
+// a()
+// b()
+// console.log(x)
+// ===========================================
+
+// var a=10;
+
+// function b(){
+//     var x=10;
+// }
+// console.log(window.a)
+// console.log(a)
+// console.log(this.a)
+// =======================================
+
+// let a;
+// console.log(a)
+
+// a=10;
+// console.log(a)
+
+// a='hello javascript'
+// console.log(a)
+// ====================================
+
+// function a(){
+//     function c(){
+//         console.log(b)
+
+//     }
+//     c()
+// }
+// var b=10;
+// a()
+// console.log(b)
+// console.log(b)
+
+// ==================================================
+
+//    var c=100
+// {
+//     var a=12;
+//     let c=24;
+//     const b=23;
+//     console.log(c)
+//     console.log(b)
+//     console.log(a)
+    
+// }
+// console.log(c)
+// ========================================
+// function z(){
+//     var b=100
+//     var l=8
+//     function x(){
+//         var c=12
+//         function y(){
+//             d=80
+//             function b(){
+//                 e=29
+//             }
+//             b()
+//             console.log(l,b,c,d,e)
+//         }
+//         y()
+//     }
+//     x()
+// }
+// z()
+// ===========================
+// function x(){
+//     console.log('aedshdgshgdsg')
+//     var i = 3
+    
+//     setTimeout(function(){
+//         console.log(i)
+
+//     },5000)
+    
+// }
+// x()
+// ==========================
+// function s(){
+//     for(let i=1; i<=5; i++){
+//         setTimeout(function(){
+//             console.log(i)
+//         },i * 2000)
+//     }
+// }
+// s()
+// ========================
+// function x(){
+//     for(var i=1;i<=5;i++){
+//         function clouser(t){
+//             setTimeout(function(){
+//                 console.log(t)
+
+//             },t*2000)
+//         }
+//         clouser(i)
+//     }
+// }
+// x()
+//====================================
+
+// function praveen(){
+//     let a=10;
+    
+//     function nanda(){
+        
+//         console.log(a)
+//         a=12;
+//     }
+//     nanda()
+//     console.log(a)
+
+// }
+// praveen()
+// ===========================================
+
+// function staement
+// function expression
+// function declaration
