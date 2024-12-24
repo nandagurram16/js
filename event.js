21f74be8ceaf388424f25397ea7af39ea1940d9f
nandagurram16 <nandagurram168@gmail.com>
2024-12-24 20:50:45 +0530

    'event'

---
+++ event.js
@@ -0,0 +1,19 @@
+
+
+// function attachEventListeners(){
+//     let count=0
+//     document.getElementById('clickme')
+//     .addEventListener('click',function xyz(){
+//         console.log('button clicked',++count)
+//     })
+// }
+// attachEventListeners()
+// ================================
+// let arr=[1,2,3,4]
+
+// console.log(arr.pop())
+// ============================
+// for(let i=1; i<=5; i++){
+    // console.log(i)
+// }
+// ============================================
