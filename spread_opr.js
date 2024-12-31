94a5c88a9ebeed10f33893376b6520e3338a5833
nandagurram16 <nandagurram168@gmail.com>
2024-12-31 13:32:54 +0530

    spread_opr

---
+++ spread_opr.js
@@ -0,0 +1,22 @@
+
+
+set1 = [1,2,3,4,5]
+
+set2 = [,6,7,8,9,10]
+
+console.log(...set1 + set2)
+
+// ex1 in object
+
+obj1 = {
+    name:'nanda',
+    eamil:'@gmail.com'
+}
+
+obj2 = {
+    ...obj1,
+    age:25
+}
+
+console.log(obj2)
+
